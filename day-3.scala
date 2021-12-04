f32b28e45beb0d32a4635aa32739cfc2d1fdcca6
Jeff Windsor <jeff.windsor@gmail.com>
2021-12-03 19:28:13 -0800

    add comments

--- day-3.scala
+++ day-3.scala
@@ -5,26 +5,31 @@ val submission_inputs = List("011000110010", "111011101000", "110100110010", "01
 val on  = '1'
 val off = '0'
 
-def bitsToDecimal(bits:Seq[Char]) = bits.reverse.zipWithIndex
-  .filter(_._1 == on)
-  .foldLeft(0)((a,i) => a + math.pow(2, i._2).toInt)
+def bitsToDecimal(bits:Seq[Char]) = bits
+  .reverse.zipWithIndex                           // reverse bit order so that index with match power of 2
+  .filter(_._1 == on)                             // filter to only on bits
+  .map { case (_,power) => math.pow(2, power) }   // each bit to decimal value of 2^n
+  .sum.toInt                                      // sum to to get base 10
 
-def mostCommon(readings: List[String]):List[Char] = readings.transpose
-  .map(_.count(_ == on))
-  .map(c => if (c >= readings.length - c ) on else off)
+def mostCommon(readings: List[String]):List[Char] = readings
+  .transpose                                                // pivot
+  .map(_.count(_ == on))                                    // count the on bits in column
+  .map(c => if (c >= readings.length - c ) on else off)     // set most common digit, tie goes to on bit
 
-def leastCommon(readings:List[String]):List[Char] = mostCommon(readings).map(c => if (c == on) off else on)
+def leastCommon(readings:List[String]):List[Char] = 
+  mostCommon(readings).map(c => if (c == on) off else on)   // invert most common
 
-def matchBits(common:List[String]=>List[Char],readings: List[String], index:Int = 0): String = readings match {
-  case List(answer) => answer
-  case _            => { val bit = common(readings)(index)
-                         matchBits(common, readings.filter(_(index) == bit), index + 1) }}
+def matchBits(f:List[String]=>List[Char],readings: List[String], index:Int = 0): String = readings match {
+  case List(answer) => answer                                                       // quit on single value
+  case _            => { val filterBit = f(readings)(index)                         // get bit to filter readings at index
+                         val filtered  = readings.filter(_(index) == filterBit)     // filter reading to only those with filterBit at index location
+                         matchBits(f, filtered, index+1 )}}                         // recurse with filtered readings and increment index 
 
 def answer1(readings: List[String]) = 
   bitsToDecimal(mostCommon(readings)) * bitsToDecimal(leastCommon(readings))
 
 def answer2(readings: List[String]) = 
-  bitsToDecimal(matchBits(mostCommon,readings)) * bitsToDecimal(matchBits(leastCommon,  readings))
+  bitsToDecimal(matchBits(mostCommon,readings)) * bitsToDecimal(matchBits(leastCommon,readings))
 
 
 println(answer1(test_inputs))
